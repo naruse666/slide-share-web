76ee06abc495b61391b3530c31af5f2f3c42b954
R1013-T <nosuke1013@icloud.com>
2024-03-23 02:22:12 +0900

    style: update nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Nav from '@/components/common/nav'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 export default function Home() {
   return (
     <main>
-      <Nav />
-      <p className="font-bold text-lg">dashboard page</p>
+      <ScrollArea className="h-dvh px-4">
+        <span className="pt-16 pb-4 block"></span>
+        <Nav />
+        <p className="font-bold text-lg">dashboard page</p>
+      </ScrollArea>
     </main>
   )
 }
