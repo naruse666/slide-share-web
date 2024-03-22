47ba7aaa1180781781a6a1e43087691ec7924985
R1013-T <nosuke1013@icloud.com>
2024-03-22 20:20:34 +0900

    style: add noto sans jp font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Noto_Sans_JP } from 'next/font/google'
 
 import { ThemeProvider } from '@/components/theme/provider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
+const notoSansJP = Noto_Sans_JP({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'LT Slide Share',
@@ -19,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={cn(notoSansJP.className, inter.className)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
